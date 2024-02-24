2e2bd7c66d8a602264ae212cecb066e7b0ca02e0
Andrii <amelchenkoan29@gmail.com>
2024-02-24 03:10:12 +0200

    gulp finished

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+import map from "./modules/map";
+import slider from "./modules/slider";
+import control_str_size from "./modules/control_str_size";
+import menu from "./modules/menu";
+
+map();
+slider();
+control_str_size();
+menu();
